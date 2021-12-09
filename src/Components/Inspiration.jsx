cbd865852153399d864a7258587126ec1696ccaa
Jayant <jayantsingh331@gmail.com>
2021-12-09 12:25:26 +0530

    insp

--- src/Components/Inspiration.jsx
+++ src/Components/Inspiration.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
-
+import React from "react";
+import dummyboy from "../Assets/dummyboyprof.svg";
+import dummygirl from "../Assets/dummygirlprof.svg";
 export default function Inspiration() {
-    return (
-        <>
-            <h1 className="text-center my-5">Page under Construction..!</h1>
-        {/* <div className="card">
-            <div className="card-items"><img src="../" alt="" /></div>
-            <div className="card-items"></div>
-        </div> */}
-        </>
-    )
+  return (
+    <>
+      <div className="wrap_inspiration">
+        <div className="card-inspiration">
+          <div className="card-items">
+            <img src={dummyboy} alt="" />
+            <p>I am very confused that which subject should I choose</p>
+          </div>
+        </div>
+        <div className="card-ispiration-reverse">
+          <div className="d-flex flex-column">
+            <div className="card-reverse-item">
+              <p>Choose the subject you like the most</p>
+              <img src={dummygirl} alt="" />
+            </div>
+          </div>
+        </div>
+        <div className="card-inspiration">
+          <div className="card-items">
+            <img src={dummyboy} alt="" />
+            <p> but I know limited subjects</p>
+          </div>
+        </div>
+        <div className="card-ispiration-reverse">
+          <div className="d-flex flex-column">
+            <div className="card-reverse-item">
+              <p>Ohh ! I get it</p>
+
+              <img src={dummygirl} alt="" />
+            </div>
+
+            <p>
+              I also faced similar problem, but after joining kaay community I
+              got to know about various Jobs of interest & you should have a
+              look at these fields & then explore your area of interest.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
